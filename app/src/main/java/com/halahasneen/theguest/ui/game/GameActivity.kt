d1f8960a6611b7ceee60c01bd3336ce162de6c01
halashasneen-oss <halashasneen@gmail.com>
2026-09-12 16:14:19 +0300

    fix: support immersive mode from Android 8 onward

--- app/src/main/java/com/halahasneen/theguest/ui/game/GameActivity.kt
+++ app/src/main/java/com/halahasneen/theguest/ui/game/GameActivity.kt
@@ -1,7 +1,9 @@
 package com.halahasneen.theguest.ui.game
 
 import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowInsetsController
 import com.halahasneen.theguest.R
@@ -15,10 +17,27 @@ class GameActivity : Activity() {
         setContentView(R.layout.activity_game)
         gameCanvas = findViewById(R.id.gameCanvas)
         findViewById<JoystickView>(R.id.joystick).onDirectionChanged = gameCanvas::setInputDirection
-        window.insetsController?.let { controller ->
-            controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-            controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        enterImmersiveMode()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun enterImmersiveMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.let { controller ->
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) enterImmersiveMode()
     }
 
     override fun onResume() {
